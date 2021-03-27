66b0680db659d687535d138344b9c396363693a5
Kevin Kamto <kamtokevin@gmail.com>
2021-03-26 20:57:10 -0400

    feat: completed list view

--- lib/main.dart
+++ lib/main.dart
@@ -10,29 +10,57 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'NFTs for Sale',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Buy my Unique Laptop as an NFT'),
-        ),
-        body: Center(
-          // Load the entire code chunk 
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
 
-// This is akin to a react component
+// This class 'StatefulWidget' is immutable & gets thrown out on reload
 class RandomWords extends StatefulWidget {
   @override
   _RandomWordsState createState() => _RandomWordsState();
 }
 
+// This is akin to a react component -- is mutable
 class _RandomWordsState extends State<RandomWords> {
-  @override
+  // private fields
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  // Private method
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd)
+            return Divider(
+              color: Colors.amber,
+            );
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  // Private method
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  // This is akin to the react build section
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List of My Stuff'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
